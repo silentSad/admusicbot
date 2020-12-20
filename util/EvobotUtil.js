e54a7dce761ddfbf620d7871f7b14ef73cebc9c6
silentSad <69804677+silentSad@users.noreply.github.com>
2020-12-20 06:50:09 +0300

    Add files via upload

--- util/EvobotUtil.js
+++ util/EvobotUtil.js
@@ -3,7 +3,7 @@ exports.canModifyQueue = (member) => {
   const botChannel = member.guild.voice.channelID;
 
   if (channelID !== botChannel) {
-    member.send("You need to join the voice channel first!").catch(console.error);
+    member.send("ادخل روم يا خبل🤪").catch(console.error);
     return;
   }
 
