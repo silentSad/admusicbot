759569f806f6bb117928211eaa79be0abe138337
silentSad <69804677+silentSad@users.noreply.github.com>
2020-12-20 07:07:20 +0300

    Delete online.js

--- online.js
+++
@@ -1,8 +0,0 @@
-/*const ex = require('express');
-const ser = ex();
-ser.all('/', (req, res)=> 
-{res.send('I am online :)')})
-function keepOnline24()
-{ser.listen(8080, ()=>
-{console.log("Online 24/7 :)")});}
-module.exports = keepOnline24;*/
